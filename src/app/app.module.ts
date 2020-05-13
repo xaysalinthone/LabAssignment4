2e48014762f23c227c1bf78d9d9b7e39f78de523
Xayaphone <xaysalinthone@gmail.com>
2020-05-12 19:09:17 -0700

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { MainContentComponent } from './main-content/main-content.component';
@@ -15,6 +15,7 @@ import { MainFooterComponent } from './main-footer/main-footer.component';
   ],
   imports: [
     BrowserModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -5,6 +5,7 @@ interface IStudent {
   firstName: string;
   lastName: string;
   course: string;
+  editMode: boolean; 
 }
 
 @Component({
@@ -14,48 +15,108 @@ interface IStudent {
 })
 export class MainContentComponent implements OnInit {
 
+  studentArray: Array<IStudent> = [];
+  disableAddButton = false;
+  constructor() { }
 
-  students: Array<IStudent> = [];
+  // students: Array<IStudent> = [];
 
-  constructor() {
-    this.students[0] = {
-      id: 1,
-      firstName: 'Xay',
-      lastName: 'Sal',
-      course: 'Programming'
-    }
-    this.students[1] = {
-      id: 1,
-      firstName: 'Bill',
-      lastName: 'Ko',
-      course: 'Math'
-    }
-    this.students[2] = {
-      id: 1,
-      firstName: 'Jill',
-      lastName: 'Reed',
-      course: 'History'
-    }
-    this.students[3] = {
-      id: 1,
-      firstName: 'Phil',
-      lastName: 'Dunphy',
-      course: 'Business'
-    }
-  }
+  // constructor() {
+  //   this.students[0] = {
+  //     id: 1,
+  //     firstName: 'Xay',
+  //     lastName: 'Sal',
+  //     course: 'Programming'
+  //   }
+  //   this.students[1] = {
+  //     id: 1,
+  //     firstName: 'Bill',
+  //     lastName: 'Ko',
+  //     course: 'Math'
+  //   }
+  //   this.students[2] = {
+  //     id: 1,
+  //     firstName: 'Jill',
+  //     lastName: 'Reed',
+  //     course: 'History'
+  //   }
+  //   this.students[3] = {
+  //     id: 1,
+  //     firstName: 'Phil',
+  //     lastName: 'Dunphy',
+  //     course: 'Business'
+  //   }
+  // }
 
   ngOnInit() {
+    this.studentArray = [
+      {
+        id: 1,
+        firstName: 'Alex',
+        lastName: 'Dunphy',
+        course: 'Bio-Chem'
+        editMode: false
+      },
+      {
+        id: 2,
+        firstName: 'Mitchel',
+        lastName: 'Pritchet',
+        course: 'Business-Law'
+        editMode: false
+
+      },
+      {
+        id: 3,
+        firstName: 'Phil',
+        lastName: 'Dunphy',
+        course: 'Business'
+        editMode: false
+
+      }
+    ];
   }
 
   addStudent() {
-    const student: IStudent = {
-      id: 1,
-      firstName: 'Alex',
-      lastName: 'Dunphy',
-      course: 'Bio-Chem'
-    };
-
-    this.students.push(student);
+    this.studentArray.unshift({
+      id: null,
+      firstName: null,
+      lastName: null,
+      course: null,
+      editMode: true
+    });
+    this.disableAddButton = true;
   }
 
+  removeStudent(index: number){
+    console.log('index---->', index);
+    this.studentArray.splice(index, 1);
+  }
+  
+  saveStudent(){
+    this.studentArray[0].editMode = false;
+    this.diableAddButton = false;
+    this.sort('asc');
+  }
+
+  sort(direction : string){
+    //const dir = direction ==='asc'?
+
+    this.studentArray.sort((a:IStudent, b:IStudent)=> {
+      //   if (a.id < b.id){
+      //     return -1;
+      //   }else{ 
+      //     return 1;
+      //   }
+      //  }else{
+      //     if (direction === 'asc'){
+      //       return 1;
+
+      //     }else{
+      //       return -1;
+      //     }
+      //     return 1;
+      //   }
+      return a.id < b.id ? -1 : 1;
+    });
+  }
 }
